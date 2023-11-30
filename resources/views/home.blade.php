2501444d267a1c89994b465e8fe2795daf746388
Sam Buckley <220099626@aston.ac.uk>
2023-11-30 12:03:59 +0000

    fix: moved css and js folders into public

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gains</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="{{asset('js/app.js')}}" defer></script>
 </head>
 <body>
     <section>
