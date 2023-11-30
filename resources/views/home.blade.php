9bb47f43b9c0a04cde690dcb84f41ffc421a2415
Sam Buckley <220099626@aston.ac.uk>
2023-11-30 10:50:08 +0000

    feat: added home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gains</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <section>
+        <header>
+    <nav class="navbar">
+        <div class="navbar__container">
+        <img src="Images/Logo.png" alt="navbar__logo"></div>
+        <div class="navbar__toggle" id="mobile-menu">
+            <span class="bar"></span> <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+        <ul class="navbar__menu">
+            <li class="navbar__item">
+                <a href="home.html" class="navbar__links">Home</a>
+            </li>
+            <li class="navbar__item">
+                <a href="Product.html" class="navbar__links">Product</a>
+            </li>
+            <li class="navbar__item">
+                <a href="About.html" class="navbar__links">About Us</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+</section>
+
+<hr class="solid">
+
+<section>
+    <div class="Featured">
+    <h1>Featured</h1>
+    <p>insert list of featured items</p>
+    <br>
+</section>
+
+    <div class="footer">
+    <footer>
+        <br>
+        <p>Team 44 | socials | email </p>
+        <br>
+    </footer>
+</div>
+</body>
+</html>
