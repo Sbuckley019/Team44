61245627ccaf4734d9b8f126a5b521027d7ac3e4
Charlie Brooker <220142483@aston.ac.uk>
2023-12-07 11:57:45 +0000

    feat: added database models

---
+++ app/Models/FavouriteProduct.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FavouriteProduct extends Model
+{
+    use HasFactory;
+    protected $table = 'favourite_products';
+
+    protected $fillable = ['customer_id', 'product_id'];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
---
+++ app/Models/OrderItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+    protected $table = 'order_items';
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'subtotal',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
