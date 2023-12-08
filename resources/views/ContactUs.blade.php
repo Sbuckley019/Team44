cd1d47652759bfefe2c510cc1562f3c61573a4cc
Jasleen Kaur <220095374@aston.ac.uk>
2023-12-08 14:14:07 +0000

    created contact us page

---
+++ resources/views/ContactUs.blade.php
@@ -0,0 +1,116 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: #f8f9fa;
+            color: #333;
+            margin: 20px;
+        }
+        .contact-container {
+            max-width: 800px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+        .section-container {
+            border: 2px solid #007bff;
+            border-radius: 10px;
+            padding: 20px;
+            margin-bottom: 20px;
+            overflow: hidden;
+            background-color: #f8f9fa; /* Light gray background */
+        }
+        .contact-details,
+        .user-input {
+            width: 100%;
+            margin-bottom: 20px;
+        }
+        .contact-details,
+        .user-input {
+            text-align: left;
+        }
+        .contact-details h2,
+        .user-input h2 {
+            color: #007bff;
+            margin-bottom: 10px;
+        }
+        .contact-details p {
+            color: #555;
+            margin: 5px 0;
+        }
+        .form-group {
+            margin-bottom: 15px;
+        }
+        .btn-submit {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        .btn-submit:hover {
+            background-color: #45a049;
+        }
+        .form-control {
+            border-radius: 5px;
+        }
+        h1 {
+            color: #007bff;
+            margin-bottom: 10px;
+        }
+        p {
+            margin: 10px 0;
+        }
+    </style>
+</head>
+<body>
+@include('Navigation')
+    <div class="contact-container">
+        <h1>Contact Us</h1>
+        <p>We are here to assist you. Reach out to us with your inquiries and feedback.</p>
+
+        <div class="section-container contact-details">
+            <h2>Contact Details:</h2>
+            <p>Email: gains@gmail.com</p>
+            <p>Phone: 01332 345475</p>
+            <p>Address: 64 Striker Lane, Kensington, London, W8 4AB</p>
+        </div>
+
+        <div class="section-container user-input">
+            <h2>Send us a message:</h2>
+            <form id="contactForm">
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message:</label>
+                    <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
+                </div>
+                <button type="button" class="btn-submit" onclick="submitForm()">Submit</button>
+            </form>
+        </div>
+    </div>
+
+    <script>
+        function submitForm() {
+            alert('Form submitted!');
+        }
+    </script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/Contact", function () {
+    return view("ContactUs");
+});
+
 Route::get('/AboutUs', function () {
     return view('AboutUs');
 });
