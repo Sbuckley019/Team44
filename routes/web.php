d4e60f84bbfa7ee98a2b7554e7c038df0475946a
hamzah-nt <220194150@aston.ac.uk>
2023-12-10 22:53:04 +0000

    basket controller and model and routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\ProductController;
 
 use App\Http\Controllers\ProductCategoryController;
 
+use App\Http\Controllers\BasketController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -41,3 +43,12 @@ Route::get('/', function () {
 Route::get('/Register', function () {
     return view('register');
 })->name('register');
+
+Route::middleware(['auth'])->group(function () {
+    Route::prefix('checkout')->group(function () {
+        Route::get('/basket', [BasketController::class, 'index'])->name('basket.index');
+        Route::post('/basket/add/{productId}', [BasketController::class, 'addProduct'])->name('basket.add');
+        Route::post('/basket/remove/{productId}', [BasketController::class, 'addProduct'])->name('basket.remove');
+        Route::post('/basket/edit/{productId}', [BasketController::class, 'editQuantity'])->name('basket.editQuantity');
+    });
+});
