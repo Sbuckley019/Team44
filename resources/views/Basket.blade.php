af9167fd1f11a76b25874a89c442c91399ed896b
Charlie Brooker <220142483@aston.ac.uk>
2024-01-23 22:56:24 +0000

    feat: edit quantity and remove in basket

--- resources/views/Basket.blade.php
+++ resources/views/Basket.blade.php
@@ -39,15 +39,34 @@
                         <div class="product-details">
                             <p>{{$item->product->product_name}}</p>
                             <small>Price £{{$item->product->price}}</small>
-                            <div class="remove-link">
-                                <a href="{{ route('home') }}">Remove</a>
+                            <form action="{{ route('basket.remove',['productId'=> $item->product->id])}}" method="post" class="remove-link">
+                                @csrf
+                                <button type="submit">Remove</button>
+                            </form>
                             </div>
                         </div>
                     </div>
 
     </td>
     <!--<td><input type="Label" class="quantity" min="1" value="{{$item->quantity}}" onchange="updateTotal()"></td>-->
-    <td><label class="quantity">{{$item->quantity}}</label>
+        <td>
+            <form action="{{ route('basket.editQuantity',['productId'=> $item->product->id]) }}" method="POST">
+                @csrf
+                <button class="minus" aria-label="Decrease by one" name="action" value="0" type="submit">
+                    <svg width="16" height="2" viewBox="0 0 16 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line y1="1" x2="16" y2="1" stroke="#0064FE" stroke-width="2" class="icon" />
+                    </svg>
+                </button>
+                <div class="number dim">{{$item->quantity}}</div>
+                <button class="plus" aria-label="Increase by one" name="action" value="1" type="submit">
+                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"
+                        class="icon">
+                        <line x1="8" y1="4.37114e-08" x2="8" y2="16" stroke="#0064FE" stroke-width="2" />
+                        <line y1="8" x2="16" y2="8" stroke="#0064FE" stroke-width="2" />
+                    </svg>
+                </button>
+            </form>
+        </td>
     <td class="subtotal">£{{($item->product->price)*$item->quantity}}</td>
     @endforeach
     @endif
