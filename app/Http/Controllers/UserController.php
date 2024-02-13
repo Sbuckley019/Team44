0e592ded379abcc8cd0a9c23deaba333187002ee
hamzah-nt <220194150@aston.ac.uk>
2024-02-13 14:30:07 +0000

    new changes with bootstrap

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -121,20 +121,22 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
+
         $request->validate([
             'username' => 'required',
             'password' => 'required',
+        ], [
+            'username.required' => 'The username field is required.',
+            'password.required' => 'The password field is required.',
         ]);
 
+        // Authenticate user
         $credentials = $request->only('username', 'password');
-
         if (Auth::attempt($credentials)) {
-
-            $basketcontroller = new BasketController();
-            $basketcontroller->guestToUser();
-
-            return redirect()->route('home')
-                ->with('success', 'Login successful');
+            return redirect()->route('home')->with('success', 'Login successful!');
+        } else {
+            // If authentication fails, redirect back with errors
+            return redirect()->back()->with('error', 'Invalid credentials');
         }
     }
 
--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="{{asset('js/app.js')}}" defer></script>
     <title>Gains</title>
 </head>
+
 <body>
     <div class="navbar">
         <div class="navbar-left">
             <img src="{{ asset('images/Logo.png') }}" height="50"> <!-- Replace with your logo -->
             <a href="{{ route('home') }}">Home</a>
             <div class="dropdown">
-                <a href="{{ route('products.index')}}" class='dropdown-item'>Products</a> 
+                <a href="{{ route('products.index')}}" class='dropdown-item'>Products</a>
                 <div class="dropdown-content">
                     <a href="{{ route('products.index',['id'=>1])}}">Mens Clothing</a>
                     <a href="{{ route('products.index',['id'=>2])}}">Womens Clothing</a>
@@ -30,7 +33,7 @@
         </div>
         <div class="navbar-right">
             @if (auth()->check())
-            <div class="dropdown"> 
+            <div class="dropdown">
                 <a class="dropdown-item name"> Hello, {{ auth()->user()->username }} </a>
                 <div class="dropdown-content">
                     <!--<a href="{{route('orders')}}">My Orders</a>!-->
@@ -45,5 +48,9 @@
             <a href="{{route('basket.index')}}"> <img src="{{ asset('images/Basket.png') }}" alt="basket" height="35"></a>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </body>
+
 </html>
