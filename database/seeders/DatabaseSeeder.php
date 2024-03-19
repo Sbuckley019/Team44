d3d37507eb39ed9307c118572ab856ad34dad905
Safian Tufail <220080680@aston.ac.uk>
2024-03-19 15:02:57 +0000

    database changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,20 +24,20 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Seed Product Categories
-        //ProductCategory::factory()->count(4)->create();
+        //ProductCategory::factory()->count(5)->create();
         // Seed products
         //Product::factory()->count(100)->create();
 
-        $this->call(ProductTableSeeder::class);
-        $this->call(ProductTableSeeder::class);
-        $this->call(ProductTableSeeder::class);
+        //$this->call(ProductTableSeeder::class);
+        //$this->call(ProductTableSeeder::class);
+        //$this->call(ProductTableSeeder::class);
 
-        //User::factory()->count(10)->create();
+        User::factory()->count(10)->create();
 
-        //Review::factory()->count(500)->create();
+        Review::factory()->count(500)->create();
 
-        //Feedback::factory()->count(10)->create();
+        Feedback::factory()->count(10)->create();
 
-        Admin::factory()->count(1)->create();
+        //Admin::factory()->count(1)->create();
     }
 }
