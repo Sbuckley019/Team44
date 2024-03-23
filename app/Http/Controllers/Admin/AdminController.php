62427758fa14aae2f80f6a899ce23e8d45657db1
Charlie Brooker <220142483@aston.ac.uk>
2024-03-23 12:47:25 +0000

    chore: fixed admin controller imports added some admin components

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
