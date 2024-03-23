b006bb7199d9e0c42d3c1d1a2025a5ee62e57edc
Charlie Brooker <220142483@aston.ac.uk>
2024-03-23 01:55:20 +0000

    chore: updated intended routing and minor css class changes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\BasketController;
+
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
+use App\Services\BasketService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -18,9 +20,15 @@ class AuthenticatedSessionController extends Controller
     /**
      * Display the login view.
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
+        if ($request->has('intended')) {
+            Session::put('url.intended', $request->input('intended'));
+        }
+
+
         return Inertia::render('Auth/Login', [
+
             'canResetPassword' => Route::has('password.request'),
             'status' => session('status'),
         ]);
@@ -35,8 +43,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        //$basketController = new BasketController();
-        //$basketController->guestToUser();
+        // $basketService = new BasketService();
+        // $basketService->guestToUser($basket);
 
         $user = Auth::user();
         if ($user->admin()->exists()) {
--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -18,7 +18,7 @@ defineExpose({ focus: () => input.value.focus() });
 </script>
 <template>
     <input
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        class="mt-1 font-roboto w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-black focus:border-black"
         v-model="model"
         ref="input"
     />
