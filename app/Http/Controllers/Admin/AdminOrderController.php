93682d61a76b358ce91f827c1ec7eb9f7541f7c7
Sultan <sultanalhail@outlook.com>
2024-03-23 22:33:53 +0000

    added draft orders table on admin page

--- app/Http/Controllers/Admin/AdminOrderController.php
+++ app/Http/Controllers/Admin/AdminOrderController.php
@@ -7,16 +7,38 @@ use App\Models\Order;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
-class AdminProductController extends Controller
+class AdminOrderController extends Controller
 {
     public function index()
-    {
-        $orders = Order::all();
-        return view('admin/orders', compact('orders'));
-    }
+{
+    
+    $orders = Order::with(['user', 'orderItems'])
+                   ->orderBy('created_at', 'desc')
+                   ->get();
+
+    
+    $orders->transform(function ($order) {
+        return [
+            'id' => $order->id,
+            'customer_name' => optional($order->user)->name, 
+            'total_price' => $order->total_price,
+            'status' => $order->status,
+            'date' => $order->created_at->toDateString(), 
+            'order_items_count' => $order->orderItems->count(), 
+        ];
+    });
+
+    return Inertia::render('Admin/Orders', ['orders' => $orders]);
+}
+
 
     public function show(Order $order)
     {
-        return view('orders', compact('order'));
+
+        $order->load('user', 'orderItems'); 
+        return Inertia::render('Admin/DetailedOrder', ['order' => $order]);
     }
+
+
+
 }
