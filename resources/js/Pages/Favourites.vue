9061405bd2f04529f7af088975c5f0785e717568
Sam Buckley <220099626@aston.ac.uk>
2024-03-24 02:21:06 +0000

    Charlie changed favourites button to look better

--- resources/js/Pages/Favourites.vue
+++ resources/js/Pages/Favourites.vue
@@ -16,8 +16,7 @@ const loginUrl = ref(`/login?intended=${encodeURIComponent(url)}`);
 <template>
     <UserLayout> <div>
                     <Header class="text-center">Log in to view favourites</Header>
-                    <img src="../../../public/images/Favourites.png" alt="Empty Basket Image" class="mx-auto mb-4 w-96" />
-                    <div class="md:px-24"><PrimaryButton >
-                    <NavLink :href="loginUrl">Catapult</NavLink></PrimaryButton></div>
+                    <img src="../../../public/images/Favourites.png" alt="Empty Favourites Image" class="mx-auto mb-4 w-96" />
+                    <div class="md:px-24 flex justify-center"><NavLink class="w-3/5 hover:border-0 active-0 " :href="loginUrl"><PrimaryButton>Go to login</PrimaryButton></NavLink></div>
             </div></UserLayout>
 </template>
